2c2de5bbbe8e8640f58be50fad36a67d811cf06c
Lucio_Beckler <luciobecklerp@gmail.com>
2023-12-29 10:45:31 -0300

    Frist commit, personalizando menu lateral

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
+import {
+    IonApp,
+    IonRouterOutlet,
+    IonSplitPane,
+    setupIonicReact,
+} from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
-import Menu from './components/Menu';
+import Menu from './components/Menu/Menu';
 import Page from './pages/Page';
 
 /* Core CSS required for Ionic components to work properly */
@@ -26,23 +31,23 @@ import './theme/variables.css';
 setupIonicReact();
 
 const App: React.FC = () => {
-  return (
-    <IonApp>
-      <IonReactRouter>
-        <IonSplitPane contentId="main">
-          <Menu />
-          <IonRouterOutlet id="main">
-            <Route path="/" exact={true}>
-              <Redirect to="/folder/Inbox" />
-            </Route>
-            <Route path="/folder/:name" exact={true}>
-              <Page />
-            </Route>
-          </IonRouterOutlet>
-        </IonSplitPane>
-      </IonReactRouter>
-    </IonApp>
-  );
+    return (
+        <IonApp>
+            <IonReactRouter>
+                <IonSplitPane contentId='main'>
+                    <Menu />
+                    <IonRouterOutlet id='main'>
+                        <Route path='/' exact={true}>
+                            <Redirect to='/folder/Inbox' />
+                        </Route>
+                        <Route path='/folder/:name' exact={true}>
+                            <Page />
+                        </Route>
+                    </IonRouterOutlet>
+                </IonSplitPane>
+            </IonReactRouter>
+        </IonApp>
+    );
 };
 
 export default App;
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,113 @@
+import {
+    IonContent,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonList,
+    IonListHeader,
+    IonMenu,
+    IonMenuToggle,
+    IonNote,
+} from '@ionic/react';
+
+import { useLocation } from 'react-router-dom';
+import {
+    qrCodeOutline,
+    notificationsOutline,
+    alertCircleOutline,
+    documentAttachOutline,
+} from 'ionicons/icons';
+import './Menu.css';
+
+interface AppPage {
+    url: string;
+    iosIcon: string;
+    mdIcon: string;
+    title: string;
+}
+
+const appPages: AppPage[] = [
+    {
+        title: 'QR Code de acesso',
+        url: '/folder/Qrcode',
+        iosIcon: qrCodeOutline,
+        mdIcon: qrCodeOutline,
+    },
+    {
+        title: 'Notificações',
+        url: '/folder/Notificacoes',
+        iosIcon: notificationsOutline,
+        mdIcon: notificationsOutline,
+    },
+    {
+        title: 'Reportar problema',
+        url: '/folder/reportarproblema',
+        iosIcon: alertCircleOutline,
+        mdIcon: alertCircleOutline,
+    },
+    {
+        title: 'Envio de documentos',
+        url: '/folder/EnvioDeDocumentos',
+        iosIcon: documentAttachOutline,
+        mdIcon: documentAttachOutline,
+    },
+];
+
+const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+const Menu: React.FC = () => {
+    const location = useLocation();
+
+    return (
+        <IonMenu contentId='main' type='overlay'>
+            <IonContent>
+                <IonList id='inbox-list'>
+                    <IonListHeader>Inbox</IonListHeader>
+                    <IonNote>hi@ionicframework.com</IonNote>
+                    {appPages.map((appPage, index) => {
+                        return (
+                            <IonMenuToggle key={index} autoHide={false}>
+                                <IonItem
+                                    className={
+                                        location.pathname === appPage.url
+                                            ? 'selected'
+                                            : ''
+                                    }
+                                    routerLink={appPage.url}
+                                    routerDirection='none'
+                                    lines='none'
+                                    detail={false}
+                                >
+                                    <IonIcon
+                                        aria-hidden='true'
+                                        slot='start'
+                                        ios={appPage.iosIcon}
+                                        md={appPage.mdIcon}
+                                    />
+                                    <IonLabel>{appPage.title}</IonLabel>
+                                </IonItem>
+                            </IonMenuToggle>
+                        );
+                    })}
+                </IonList>
+
+                {/*Ion-list de label desnecessária */}
+                {/* <IonList id='labels-list'>
+                    <IonListHeader>Labels</IonListHeader>
+                    {labels.map((label, index) => (
+                        <IonItem lines='none' key={index}>
+                            <IonIcon
+                                aria-hidden='true'
+                                slot='start'
+                                icon={documentAttachOutline}
+                            />
+                            <IonLabel>{label}</IonLabel>
+                        </IonItem>
+                    ))}
+                </IonList> */}
+            </IonContent>
+        </IonMenu>
+    );
+};
+
+export default Menu;
